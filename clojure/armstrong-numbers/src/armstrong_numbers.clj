e41192c1f16db46d1712f43a5b2b47fd48d5ee84
Pratik Desai <pdesai@ricelake.com>
2020-02-04 22:22:01 -0600

    Code improvements for armstrong numbers post feedback.

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,22 +1,15 @@
 (ns armstrong-numbers)
 
-(defn number-of-digits [num]
-  (count (str num)))
+(defn- digits [num]
+  (mapv #(Character/getNumericValue %) (str num)))
 
-(defn char->int [chr]
-  (Integer/parseInt (str chr)))
+(defn- exp [n x]
+  (loop [acc 1 n n]
+    (if (zero? n) acc
+        (recur (* x acc) (dec n)))))
 
-(defn num->num-seq [num]
-  (map char->int (str num)))
-
-(defn pow [p v]
-  (reduce *' (repeat p v)))
-
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-  (let [number-of-digits (number-of-digits num)
-        num-seq (num->num-seq num)
-        raised-pow (map (partial pow number-of-digits) num-seq)
-        summed-raised-pow (apply + raised-pow)]
-    (= num summed-raised-pow)))
-
+(defn armstrong? [num]
+  (->> (digits num)
+       (#(map (partial exp (count %)) %))
+       (apply +)
+       (= num)))
