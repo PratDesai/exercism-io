fb0fc79633ea11766e3d0ddafc080eb3cfb83e2c
Pratik Desai <pdesai@ricelake.com>
2020-02-06 23:39:38 -0600

    Improvements to armstrong numbers post review.

--- clojure/armstrong-numbers/project.clj
+++ clojure/armstrong-numbers/project.clj
@@ -1,5 +1,6 @@
 (defproject armstrong-numbers "0.1.0-SNAPSHOT"
   :description "armstrong-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/armstrong-numbers"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,19 +1,12 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  (:require [clojure.math.numeric-tower :as math]))
 
 (defn- digits [num]
   (mapv #(Character/getNumericValue %) (str num)))
 
-(defn- exp [n x]
-  (loop [acc 1 n n base x]
-    (cond
-      (zero? n) acc
-      (even? n) (recur acc (/ n 2) (*' base base))
-      :else (recur (*' acc base) (/ (- n 1) 2) (*' base base)))))
-
 (defn armstrong? [num]
   (let [digits (digits num)
-        num-digits (count digits)
-        digits-raised-to-pow (map #(exp num-digits %) digits)]
-    (->> digits-raised-to-pow
+        num-digits (count digits)]
+    (->> (map #(math/expt % num-digits) digits)
          (apply +)
          (= num))))
