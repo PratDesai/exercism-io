2537c3002cc4a92fa62b4e418598f3484d4c7962
Pratik Desai <pdesai@ricelake.com>
2020-02-20 12:04:31 -0600

    Improved code for allergies.

--- clojure/allergies/src/allergies.clj
+++ clojure/allergies/src/allergies.clj
@@ -1,16 +1,19 @@
 (ns allergies)
 
-(def allergy-data {:eggs 1
-                   :peanuts 2
-                   :shellfish 4
-                   :strawberries 8
-                   :tomatoes 16
-                   :chocolate 32
-                   :pollen 64
-                   :cats 128})
+(def all-allergies {:eggs 1
+                    :peanuts 2
+                    :shellfish 4
+                    :strawberries 8
+                    :tomatoes 16
+                    :chocolate 32
+                    :pollen 64
+                    :cats 128})
 
 (defn allergic-to? [score allergy]
-  (not (zero? (bit-and score (allergy allergy-data)))))
+  (->> (allergy all-allergies)
+       (bit-and score)
+       zero?
+       not))
 
 (defn allergies [score]
-  (filterv #(allergic-to? score %) (keys allergy-data)))
+  (filterv #(allergic-to? score %) (keys all-allergies)))
