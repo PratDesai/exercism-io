a64539a6deb26bac77f6e4cd832145593177d642
Pratik Desai <pdesai@ricelake.com>
2020-02-20 22:23:57 -0600

    Added unique name check for new robot.

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -8,11 +8,19 @@
   (apply str (take n (repeatedly #(rand-nth coll)))))
 
 (defn- create-name []
-  (str (take-rand alphabets 2) 
-       (take-rand numbers 3)))
+   (str (take-rand alphabets 2) 
+        (take-rand numbers 3)))
+
+(defn- is-unique-name? [name]
+  (not-any? #(= % name) (mapcat #(vals @%) @robots)))
+
+(defn- create-unique-name []
+  (->> (take 10 (repeatedly create-name))
+       (filter is-unique-name?)
+       first))
 
 (defn robot []
-  (let [robot (atom {:name (create-name)})]
+  (let [robot (atom {:name (create-unique-name)})]
     (swap! robots #(conj % robot))
     robot))
 
@@ -20,4 +28,4 @@
   (:name @robot))
 
 (defn reset-name [robot]
-  (reset! robot {:name (create-name)}))
+  (reset! robot {:name (create-unique-name)}))
