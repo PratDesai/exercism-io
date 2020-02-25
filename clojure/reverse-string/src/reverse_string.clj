6c9a2e3f8c2bf7b90ac37c1373fc5d75efdb81fc
Pratik Desai <pdesai@ricelake.com>
2020-02-24 22:02:41 -0600

    Improved reverse string solution.

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,9 +1,4 @@
 (ns reverse-string)
 
-(defn reverse-string [s] 
-  (->> 
-   (loop [acc '() s s]
-     (if (empty? s) 
-       acc 
-       (recur (conj acc (first s)) (rest s))))
-   (apply str)))
+(defn reverse-string [s]
+  (apply str (reduce conj '() s)))
