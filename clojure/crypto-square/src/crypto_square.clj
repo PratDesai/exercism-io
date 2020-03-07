bb9e6de77a1af44316a91b26219c4a6acf92480d
Pratik Desai <pdesai@ricelake.com>
2020-03-07 00:46:39 -0600

    Updated crypto square code for more code reuse.

--- clojure/crypto-square/src/crypto_square.clj
+++ clojure/crypto-square/src/crypto_square.clj
@@ -1,8 +1,7 @@
 (ns crypto-square)
 
 (defn- digit-alphabet? [c]
-  (or (Character/isDigit c)
-      (Character/isLetter c)))
+  (or (Character/isDigit c) (Character/isLetter c)))
 
 (defn- transpose [coll]
   (->> (apply mapv vector coll)
@@ -17,23 +16,21 @@
 
 (defn plaintext-segments 
   ([s] (plaintext-segments s nil))
-  ([s padding] 
-   (let [norm-text (normalize-plaintext s)
-         n (square-size norm-text)]
-     (->> (partition n n padding norm-text)
-          (mapv #(apply str %))))))
+  ([s pad] (let [norm-text (normalize-plaintext s)
+                 n (square-size norm-text)]
+             (->> (partition n n pad norm-text)
+                  (mapv #(apply str %))))))
 
-(defn- ciphertext-segments [s]
-  (->> (plaintext-segments s (repeat \space))
-       (transpose)))
+(defn- ciphertext-segments
+  ([s] (ciphertext-segments s nil))
+  ([s c] (->> (plaintext-segments s (repeat \space))
+              (transpose)
+              (map #(.trim %))
+              (interpose c)
+              (apply str))))
 
 (defn ciphertext [s]
-  (->> (ciphertext-segments s)
-       (map #(.trim %))
-       (apply str)))
+  (ciphertext-segments s))
 
 (defn normalize-ciphertext [s] 
-  (->> (ciphertext-segments s)
-       (map #(.trim %))
-       (interpose \space)
-       (apply str)))
+  (ciphertext-segments s \space))
