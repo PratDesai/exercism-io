b6b5bf377792984cb31cae56820b0258aa0d182d
Pratik Desai <pdesai@ricelake.com>
2020-03-06 23:33:00 -0600

    Updated octal to make use of regex.

--- clojure/octal/src/octal.clj
+++ clojure/octal/src/octal.clj
@@ -3,18 +3,17 @@
 (defn- char->digit [c]
   (Character/getNumericValue c))
 
-(defn- not-octal? [s]
-  (some #(or (not (Character/isDigit %))
-             (> (char->digit %) 7)) s))
+(defn- octal? [s]
+  (re-matches #"[0-7]*" s))
 
 (defn- pow-seq [x]
-  (map #(reduce *'  (repeat % x)) (range)))
+  (map #(reduce *' (repeat % x)) (range)))
 
 (defn to-decimal [s]
-  (cond
-    (not-octal? s) 0
-    :else (->>
-           (reverse s)
-           (map char->digit)
-           (map * (pow-seq 8))
-           (reduce +))))
+  (if (octal? s)
+    (->>
+     (reverse s)
+     (map char->digit)
+     (map * (pow-seq 8))
+     (reduce +))
+    0))
