ce4357884d5829eac2ff6893e9d0f4858ac9c61a
Pratik Desai <pdesai@ricelake.com>
2020-04-20 15:21:18 -0500

    Second solution for series.

--- clojure/series/src/series.clj
+++ clojure/series/src/series.clj
@@ -1,10 +1,7 @@
 (ns series)
 
 (defn slices [string length]
-  (cond (empty? string) []
-        (zero? length) [""]
-        (> length (count string)) []
-        :else (loop [acc [] s string]
-                (if (< (count s) length)
-                  acc
-                  (recur (conj acc (apply str (take length s))) (rest s))))))
+  (cond 
+    (zero? length) [""]
+    :else (->> (partition length 1 string)
+               (map #(apply str %)))))
