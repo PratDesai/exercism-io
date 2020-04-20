0fb1a179ab28dba58e2f78429948f5561b1da08c
Pratik Desai <pdesai@ricelake.com>
2020-04-19 22:20:53 -0500

    Second solution for raindrops. Refactored to make it more data driven and easily extensible.

--- clojure/raindrops/src/raindrops.clj
+++ clojure/raindrops/src/raindrops.clj
@@ -1,14 +1,14 @@
 (ns raindrops)
 
+(def factors {3 "Pling"
+              5 "Plang"
+              7 "Plong"})
+
 (defn check-factor [div response num]
-  (when (= 0 (rem num div))
+  (when (zero? (rem num div))
     response))
 
 (defn convert [num]
-  (let [check-factors (juxt (partial check-factor 3 "Pling")
-                            (partial check-factor 5 "Plang")
-                            (partial check-factor 7 "Plong"))
-        response (apply str (check-factors num))]
-    (if (empty? response)
-      (str num)
-      response)))
+  (->> (map #(check-factor (key %) (val %) num) factors)
+       (apply str)
+       (#(if (empty? %) (str num) %))))
