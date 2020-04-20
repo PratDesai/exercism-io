30b36582150909952618fda9ea899ad9f0a809a4
Pratik Desai <pdesai@ricelake.com>
2020-04-20 08:58:14 -0500

    Second solution for rna transcription.

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,10 +1,11 @@
 (ns rna-transcription)
 
-(def dna-to-rna {"C" "G"
-                 "G" "C"
-                 "A" "U"
-                 "T" "A"})
+(def dna-to-rna {\C \G
+                 \G \C
+                 \A \U
+                 \T \A})
 
 (defn to-rna [dna]
-  (->> (map #(or (dna-to-rna (str %)) (throw (AssertionError.))) dna)
+  (->> (map #(or (dna-to-rna %) 
+                 (throw (AssertionError.))) dna)
        (apply str)))
