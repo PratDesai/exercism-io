a746c2e5e24e3c521a10398a820a442f970044a7
Pratik Desai <pdesai@ricelake.com>
2020-10-16 16:29:00 -0500

    A more concise solution for phone-number.

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -3,32 +3,18 @@
 (def ^:private invalid-response "0000000000")
 (def ^:private minimum-digits 10)
 
-(defn- digits-only [num-string]
-  (->> num-string
-       (re-seq #"\d+")
-       (apply str)))
-
-(defn- validate-number [number]
-  (let [num-digits (count number)]
-    (cond
-      (< num-digits minimum-digits) invalid-response
-      (= num-digits minimum-digits) number
-      :else (if (= \1 (first number))
-              (apply str (rest number))
-              invalid-response))))
-
 (defn number [num-string]
-  (->> num-string
-       digits-only
-       validate-number))
+  (let [digits (apply str (re-seq #"\d+" num-string))
+        num-digits (count digits)]
+    (cond (< num-digits minimum-digits) invalid-response
+          (= num-digits minimum-digits) digits
+          :else (if (= \1 (first digits))
+                  (subs digits 1)
+                  invalid-response))))
 
 (defn area-code [num-string]
-  (->> num-string
-       number
-       (re-find #"\d{3}")))
+  (subs (number num-string) 0 3))
 
 (defn pretty-print [num-string]
-  (->> num-string
-       number
-       ((comp rest #(re-find #"(\d{3})(\d{3})(\d{4})" %)))
-       (#(str "(" (first %) ") " (first (rest %)) "-" (last %)))))
+  (->> (number num-string)
+       (#(str "(" (subs % 0 3) ") " (subs % 3 6) "-" (subs % 6 10)))))
