db9ec176b80f2f295adcc9eb344539b14dbf521d
Pratik Desai <pdesai@ricelake.com>
2020-10-16 10:58:20 -0500

    Added first solution for run-length-encoding.

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,9 +1,24 @@
-(ns run-length-encoding)
+(ns run-length-encoding
+  (:require [clojure.edn :as edn]))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (let [char-groups (map first (re-seq #"(.)\1+|." plain-text))]
+    (->> char-groups
+         (map #(if (> (count %) 1)
+                 (str (count %) (first %))
+                 %))
+         (apply str))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [cipher-text])
+  [cipher-text]
+  (let [encoded-groups (re-seq #"\d+.|." cipher-text)
+        read-first-number (comp edn/read-string #(re-find #"\d+" %))]
+    (->> encoded-groups
+         (map
+          #(if (number? (read-first-number %))
+             (apply str (repeat (read-first-number %) (last %)))
+             %))
+         (apply str))))
