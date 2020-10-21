2367253e8185adfe6583823aec337ff45451ed40
Pratik Desai <pdesai@ricelake.com>
2020-10-21 14:12:10 -0500

    Improved run length encoding decode function.

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,17 +1,24 @@
 (ns run-length-encoding)
 
+(defn- encode-run [run]
+  (let [length (count run)]
+    (if (<= length 1) run [length (first run)])))
+
+(defn- decode-group [[_ g1 g2]]
+  (if (empty? g1) g2 (apply str (repeat (Integer. g1) g2))))
+
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (->> (partition-by identity plain-text)
-       (mapcat #(if (<= (count %) 1) % [(count %) (first %)]))
+  (->> plain-text
+       (partition-by identity)
+       (mapcat encode-run)
        (apply str)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  (letfn [(decode [[match g1 g2]]
-                  (if (nil? g1) match (apply str (repeat (Integer. g1) g2))))]
-    (->> (re-seq #"(\d+)(.)|(.)" cipher-text)
-         (map decode)
-         (apply str))))
+  (->> cipher-text 
+       (re-seq #"(\d*)(\D)")
+       (map decode-group)
+       (apply str)))
