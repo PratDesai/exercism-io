1a44a59a392ece3d53416cfa5e8dea736c1b6f44
Pratik Desai <pdesai@ricelake.com>
2020-10-20 22:52:40 -0500

    Run length encoding now makes use of partition-by for encoding and regex groups for decoding.

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,24 +1,17 @@
-(ns run-length-encoding
-  (:require [clojure.edn :as edn]))
+(ns run-length-encoding)
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (let [char-groups (map first (re-seq #"(.)\1*" plain-text))]
-    (->> char-groups
-         (map #(if (> (count %) 1)
-                 (str (count %) (first %))
-                 %))
-         (apply str))))
+  (->> (partition-by identity plain-text)
+       (mapcat #(if (<= (count %) 1) % [(count %) (first %)]))
+       (apply str)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  (let [encoded-groups (re-seq #"\d+.|." cipher-text)
-        read-first-number (comp edn/read-string #(re-find #"\d+" %))]
-    (->> encoded-groups
-         (map
-          #(if (number? (read-first-number %))
-             (apply str (repeat (read-first-number %) (last %)))
-             %))
+  (letfn [(decode [[match g1 g2]]
+                  (if (nil? g1) match (apply str (repeat (Integer. g1) g2))))]
+    (->> (re-seq #"(\d+)(.)|(.)" cipher-text)
+         (map decode)
          (apply str))))
