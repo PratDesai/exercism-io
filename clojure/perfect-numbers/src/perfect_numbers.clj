7728c8ebb40772c12249d56314bd0fc6be65a61c
Pratik Desai <pdesai@ricelake.com>
2020-10-30 23:12:00 -0500

    Added solution to perfect number.

--- clojure/perfect-numbers/src/perfect_numbers.clj
+++ clojure/perfect-numbers/src/perfect_numbers.clj
@@ -1,5 +1,12 @@
 (ns perfect-numbers)
 
-(defn classify [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn factors [n]
+  (filter #(zero? (mod n %)) (range 1 n)))
+
+(defn classify [n]
+  (if (neg? n)
+    (throw (IllegalArgumentException.))
+    (let [sum (apply + (factors n))]
+      (cond (= sum n) :perfect
+            (< sum n) :deficient
+            :else :abundant))))
