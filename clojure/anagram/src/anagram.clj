b68c85d610a09d90f165723045ab36e5609e04a9
Pratik Desai <pdesai@ricelake.com>
2020-11-11 23:03:32 -0600

    Formatting fix for anagram.

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,15 +1,15 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn- not-same? [word propect]
-  (not= word (str/lower-case propect)))
+(defn- not-same? [lower-case-word propect]
+  (not= lower-case-word (str/lower-case propect)))
 
-(defn- match? [word-fs prospect]
+(defn- fs-match? [word-fs prospect]
   (= word-fs (frequencies (str/lower-case prospect))))
 
-(defn anagrams-for [word prospect-list]
-  (let [word-lower-case (str/lower-case word)
-        word-fs (frequencies word-lower-case)
-        anagram? (every-pred (partial not-same? word-lower-case)
-                                    (partial match? word-fs))]
+(defn anagrams-for [lower-case-word prospect-list]
+  (let [lower-case-word (str/lower-case lower-case-word)
+        word-fs (frequencies lower-case-word)
+        anagram? (every-pred (partial not-same? lower-case-word)
+                             (partial fs-match? word-fs))]
     (filter anagram? prospect-list)))
