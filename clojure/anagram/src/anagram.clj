84265ee9b11361b6840670554bd6e43e3f87d3e9
Pratik Desai <pdesai@ricelake.com>
2020-11-13 00:34:39 -0600

    Updated anagram solution post review.

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,15 +1,12 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn- not-same? [lower-case-word propect]
-  (not= lower-case-word (str/lower-case propect)))
+(defn- anagram? [word-lc word-fs prospect]
+  (let [prospect-lc (str/lower-case prospect)]
+    (and (not= word-lc prospect-lc)
+         (= word-fs (frequencies prospect-lc)))))
 
-(defn- fs-match? [word-fs prospect]
-  (= word-fs (frequencies (str/lower-case prospect))))
-
-(defn anagrams-for [lower-case-word prospect-list]
-  (let [lower-case-word (str/lower-case lower-case-word)
-        word-fs (frequencies lower-case-word)
-        anagram? (every-pred (partial not-same? lower-case-word)
-                             (partial fs-match? word-fs))]
-    (filter anagram? prospect-list)))
+(defn anagrams-for [word prospect-list]
+  (let [word-lc (str/lower-case word)
+        word-fs (frequencies word-lc)]
+    (filter (partial anagram? word-lc word-fs) prospect-list)))
