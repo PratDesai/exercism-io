0af0a95f8e8879f8c1665f20694c366a0aa8cc83
Pratik Desai <pdesai@ricelake.com>
2020-12-28 21:22:30 -0600

    Added solution for isogram in csharp track.

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+  public static bool IsIsogram(string word)
+  {
+    var normalized = Normalize(word);
+    return normalized.Count() == normalized.Distinct().Count();
+  }
+
+  private static string Normalize(string val)
+  {
+    return val.Replace("-", "").Replace(" ", "").ToLower();
+  }
+}
