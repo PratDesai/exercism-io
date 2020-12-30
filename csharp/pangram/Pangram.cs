4abc1ceb61257bcb43f085bfb3d5440fcae170ea
Pratik Desai <pdesai@ricelake.com>
2020-12-29 22:57:55 -0600

    Added solution for pangram in csharp track.

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+  public static bool IsPangram(string input)
+  {
+    var pattern = new Regex("[^a-z]");
+    return pattern.Replace(input.ToLower(), "").Distinct().Count() == 26;
+  }
+}
