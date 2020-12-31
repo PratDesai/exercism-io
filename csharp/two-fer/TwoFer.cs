666584f7f65204fcf775cca79e315518ad110ad8
Pratik Desai <pdesai@ricelake.com>
2020-12-31 16:25:28 -0600

    Updated TwoFer in csharp track post mentor review.

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -1,7 +1,4 @@
 ﻿public static class TwoFer
 {
-    public static string Speak(string name = "you")
-    {
-        return $"One for {name}, one for me.";
-    }
+    public static string Speak(string name = "you") => $"One for {name}, one for me."; 
 }
