4b7ca6fe66ebf60bfa1c5305fdad63fadb0c595d
Pratik Desai <pdesai@ricelake.com>
2020-12-30 18:16:21 -0600

    Added solution for reverse string in csharp track.

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        // return string.Concat(input.Reverse());
+        return input.Select(x => $"{x}").Aggregate("", (acc, s) => s + acc);
+    }
+}
