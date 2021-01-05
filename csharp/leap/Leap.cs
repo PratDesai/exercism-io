3ea7fb3518f07288e0f63c110d7e8dc57cb83eee
Pratik Desai <pdesai@ricelake.com>
2021-01-04 20:47:43 -0600

    Updated solution for leap in csharp track.

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,9 +2,6 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        return !(year % 100 == 0 && year % 400 != 0) &&
-               year % 4 == 0;
-    }
+    public static bool IsLeapYear(int year) =>
+            !(year % 100 == 0 && year % 400 != 0) && year % 4 == 0;
 }
