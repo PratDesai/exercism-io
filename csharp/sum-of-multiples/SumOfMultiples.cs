95c0de972b866de0f5f4d470d4f7fccf70289568
Pratik Desai <pdesai@ricelake.com>
2021-01-09 23:38:57 -0600

    Added solution for sum of multiples in csharp track.

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var isMultiple = IsMultiple(multiples.Where(x => x > 0));
+        return Enumerable.Range(0, max).Where(x => isMultiple(x)).Sum();
+    }
+
+    private static Func<int, bool> IsMultiple(IEnumerable<int> multiples)
+    {
+        return num => multiples.Any(mult => num % mult == 0);
+    }
+}
