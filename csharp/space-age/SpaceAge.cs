4b1dfe27de63499cd4a044db4e25b808b522dbdb
Pratik Desai <PDesai@RiceLake.com>
2021-01-18 23:03:08 -0600

    Updated solution for space age in csharp track.

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -9,43 +9,19 @@ public class SpaceAge
         m_toPlanetYears = secondsPerYear => seconds / secondsPerYear;
     }
 
-    public double OnEarth()
-    {
-        return m_toPlanetYears(31557600);
-    }
+    public double OnEarth() => m_toPlanetYears(31557600);
+    
+    public double OnMercury() => m_toPlanetYears(7600543.81992);
 
-    public double OnMercury()
-    {
-        return m_toPlanetYears(7600543.81992);
-    }
+    public double OnVenus() => m_toPlanetYears(1.9414149052176E7);
 
-    public double OnVenus()
-    {
-        return m_toPlanetYears(1.9414149052176E7);
-    }
+    public double OnMars() => m_toPlanetYears(5.9354032690079994E7);
 
-    public double OnMars()
-    {
-        return m_toPlanetYears(5.9354032690079994E7);
-    }
+    public double OnJupiter() => m_toPlanetYears(3.74355659124E8);
 
-    public double OnJupiter()
-    {
-        return m_toPlanetYears(3.74355659124E8);
-    }
+    public double OnSaturn() => m_toPlanetYears(9.292923628848E8);
 
-    public double OnSaturn()
-    {
-        return m_toPlanetYears(9.292923628848E8);
-    }
+    public double OnUranus() => m_toPlanetYears(2.6513700193296E9);
 
-    public double OnUranus()
-    {
-        return m_toPlanetYears(2.6513700193296E9);
-    }
-
-    public double OnNeptune()
-    {
-        return m_toPlanetYears(5.200418560032001E9);
-    }
+    public double OnNeptune() => m_toPlanetYears(5.200418560032001E9);
 }
