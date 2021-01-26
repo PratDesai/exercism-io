42413ac767f3efecefb7f8d2186be60b1a730561
Pratik Desai <PDesai@RiceLake.com>
2021-01-25 20:34:07 -0600

    Updated solution to high scores in csharp track.

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -3,19 +3,19 @@ using System.Linq;
 
 public class HighScores
 {
-    private List<int> m_scores;
+    private List<int> m_highScores;
     
     public HighScores(List<int> list)
     {
-        m_scores = list;
+        m_highScores = list;
     }
 
-    public List<int> Scores() => m_scores;
+    public List<int> Scores() => m_highScores;
 
-    public int Latest() => m_scores.Last();
+    public int Latest() => m_highScores.Last();
 
-    public int PersonalBest() => m_scores.Max();
+    public int PersonalBest() => m_highScores.Max();
 
     public List<int> PersonalTopThree() => 
-        m_scores.OrderByDescending(x => x).Take(3).ToList();
+        m_highScores.OrderByDescending(x => x).Take(3).ToList();
 }
