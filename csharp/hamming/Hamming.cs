bbd8c7d35bbef5139ed49c386436a3ae86f00db4
Pratik Desai <PDesai@RiceLake.com>
2021-01-27 21:11:27 -0600

    Updated solution for hamming in csharp track

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 
 public static class Hamming
@@ -8,28 +6,10 @@ public static class Hamming
     public static int Distance(string firstStrand, string secondStrand)
     {
         if (firstStrand.Count() != secondStrand.Count())
-        {
-            throw new ArgumentException();
-        }
-        var mismatchAccumulator = new MismatchAccumulator();
-        firstStrand.SequenceEqual(secondStrand, mismatchAccumulator);
-        return mismatchAccumulator.Count;
-    }
+            throw new ArgumentException("Strand length mismatch");
 
-    private class MismatchAccumulator : EqualityComparer<char>
-    {
-        public int Count { get; private set; }
-
-        public override bool Equals(char x, char y)
-        {
-            if (!x.Equals(y))
-            {
-                Count++;
-            }
-            return true;
-        }
-
-        public override int GetHashCode(char obj) =>
-            throw new NotImplementedException();
+        return firstStrand.Select((x, idx) => x.Equals(secondStrand[idx]))
+                          .Where(x => !x)
+                          .Count();
     }
 }
