ea20daf3f9497ebc38efb4603619f87a0581e707
Pratik Desai <pdesai@ricelake.com>
2021-04-05 18:18:15 -0500

    Added solution for leap in fsharp track.

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,8 @@
+﻿module Leap
+
+let leapYear (year: int): bool = 
+    let divisibleBy4 = year % 4 = 0
+    let divisibleBy100 = year % 100 = 0
+    let divisibleBy400 = year % 400 = 0
+    divisibleBy4 && not (divisibleBy100 && not divisibleBy400)
+
