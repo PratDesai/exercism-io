c162bc3a6ee346cb21d16384f243f0862c5a986b
Pratik Desai <pdesai@ricelake.com>
2021-04-05 10:10:46 -0500

    Added solution to two-fer in fsharp track.

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,7 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string =
+    let response x = $"One for {x}, one for me."
+    match input with
+    | Some x -> response x 
+    | None -> response "you"
