6276d6a3e4559dc0a59bd27eb52dec7d646a186d
Pratik Desai <pdesai@ricelake.com>
2021-04-08 11:14:39 -0500

    Updated solution for grains in fsharp track.

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -3,9 +3,11 @@
 let square =
     function
     | x when x < 1 || x > 64 -> Error "square must be between 1 and 64"
-    | x -> Ok(pown 2UL ((int x) - 1))
+    | x -> Ok(1UL <<< x - 1)
 
-let total : Result<uint64, string> =
+let total : Result<uint64, string> = Ok System.UInt64.MaxValue
+
+let totalCalculated : Result<uint64, string> =
     let extract =
         function
         | Ok x -> x
@@ -16,27 +18,17 @@ let total : Result<uint64, string> =
     |> List.sumBy extract
     |> Ok
 
-(* The following total function with error check is not necessary as the list
-of integers mapped with the square function is generated internally,
-and is within the acceptable input range of numbers of the square function. *)
-let totalWithErrorCheck : Result<uint64, string> =
-    let extract =
-        function
-        | Ok x -> x
-        | Error _ -> 0UL
-
-    let squares = List.map square [ 1 .. 64 ]
-
-    match squares with
-    | list when
-        List.forall
-            (function
-            | Ok _ -> true
-            | Error _ -> false)
-            list -> list |> List.sumBy extract |> Ok
-    | list ->
-        List.find
-            (function
-            | Error _ -> true
-            | _ -> false)
-            list
+(*  The following function (sourced from mazhuravlev's solution) 
+    with error propagation is not strictly necessary as the list 
+    of integers mapped with the square function is generated 
+    internally, and is within the acceptable input range 
+    of numbers for the square function. *)
+let totalCalculatedWithErrorPropagation : Result<uint64, string> =
+    [ 1 .. 64 ]
+    |> List.map square
+    |> List.reduce
+        (fun a b ->
+            match (a, b) with
+            | (Ok a, Ok b) -> Ok(a + b)
+            | (Error a, _) -> Error a
+            | (_, Error b) -> Error b)
