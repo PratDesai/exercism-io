2877d9f2b9173c2f90c2ea255bd3d6bc259363c6
Pratik Desai <pdesai@ricelake.com>
2021-04-19 10:02:39 -0500

    Updated queen-attack solution for fsharp track post mentor feedback.

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -6,7 +6,16 @@ let create =
     | (_, y) when y < 0 || y > 7 -> false
     | _ -> true
 
-let canAttack (q1x: int, q1y: int) (q2x: int, q2y: int) =
+let canAttack =
+    function
+    | (q1x, q1y) ->
+        function
+        | (q2x, _) when q1x = q2x -> true
+        | (_, q2y) when q1y = q2y -> true
+        | (q2x, q2y) when abs (q1x - q2x) = abs (q1y - q2y) -> true
+        | _ -> false
+
+let canAttackFirstSolution (q1x: int, q1y: int) (q2x: int, q2y: int) =
     match () with
     | () when q1x = q2x -> true
     | () when q1y = q2y -> true
