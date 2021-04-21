2f8a61c1d973a9518600158dc013408cc9e2a4ac
Pratik Desai <pdesai@ricelake.com>
2021-04-20 22:07:03 -0500

    Final mentor approved solution for queen-attack in fsharp track.

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -12,19 +12,3 @@ let canAttack (q1x, q1y) (q2x, q2y) =
     | _, 0 -> true
     | dx, dy when abs (dx) = abs (dy) -> true
     | _ -> false
-
-let canAttackSecondSolution =
-    function
-    | (q1x, q1y) ->
-        function
-        | (q2x, _) when q1x = q2x -> true
-        | (_, q2y) when q1y = q2y -> true
-        | (q2x, q2y) when abs (q1x - q2x) = abs (q1y - q2y) -> true
-        | _ -> false
-
-let canAttackFirstSolution (q1x: int, q1y: int) (q2x: int, q2y: int) =
-    match () with
-    | () when q1x = q2x -> true
-    | () when q1y = q2y -> true
-    | () when abs (q1x - q2x) = abs (q1y - q2y) -> true
-    | _ -> false
