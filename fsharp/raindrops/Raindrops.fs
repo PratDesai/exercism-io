15324394b383be9e4be2efe1f30400af78e66331
Pratik Desai <pdesai@ricelake.com>
2021-04-21 23:17:41 -0500

    Added solution for raindrops in fsharp track.

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,18 @@
+﻿module Raindrops
+
+let rainDropSounds =
+    [ (3, "Pling")
+      (5, "Plang")
+      (7, "Plong") ]
+
+let convert (number: int) : string =
+    let isRainSound (f, _) = number % f = 0
+    let getSound (_, s) = s
+
+    rainDropSounds
+    |> List.filter isRainSound
+    |> List.map getSound
+    |> List.fold (+) ""
+    |> function
+    | "" -> $"{number}"
+    | s -> s
