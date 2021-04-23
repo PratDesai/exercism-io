6c10cd5082a75691508d47e7029d3b0026c5d136
Pratik Desai <pdesai@ricelake.com>
2021-04-23 10:18:51 -0500

    Added solution for hamming in fsharp track.

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,12 @@
+﻿module Hamming
+
+let allNuleotides s = String.forall "CAGT".Contains s
+
+let distance (s1: string) (s2: string) : int option =
+    match (s1.Length = s2.Length, allNuleotides (s1), allNuleotides (s2)) with
+    | (false, _, _) -> None
+    | (_, false, _) -> None
+    | (_, _, false) -> None
+    | _ ->
+        Seq.map2 (<>) s1 s2
+        |> (Seq.filter id >> Seq.length >> Some)
