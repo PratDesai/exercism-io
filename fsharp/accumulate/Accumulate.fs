098186f2472ffb2e81454a0d9d5fa0637212b696
Pratik Desai <pdesai@ricelake.com>
2021-05-03 10:11:52 -0500

    Added solution for accumulate in fsharp track.

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let rec accumulate (func: 'a -> 'b) (input: 'a list) : 'b list =
+    let rec map (mapFunc: 'a -> 'b) (input: 'a list) (acc: 'b list) =
+        match input with
+        | [] -> List.rev acc
+        | x :: rest -> map mapFunc rest ((func x) :: acc)
+
+    map func input []
