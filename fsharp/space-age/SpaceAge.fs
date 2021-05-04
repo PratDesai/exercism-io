ba4cc984c6646abc027e8b549dc778189af61c29
Pratik Desai <pdesai@ricelake.com>
2021-05-04 18:00:07 -0500

    Updated solution for space age based on mentor review. Made use of units of measure.

--- fsharp/space-age/SpaceAge.fs
+++ fsharp/space-age/SpaceAge.fs
@@ -1,5 +1,11 @@
 ﻿module SpaceAge
 
+[<Measure>]
+type earthYear
+
+[<Measure>]
+type second
+
 type Planet =
     | Mercury
     | Venus
@@ -13,13 +19,13 @@ type Planet =
 let age (planet: Planet) (seconds: int64) : float =
     planet
     |> function
-    | Mercury -> 0.2408467
-    | Venus -> 0.61519726
-    | Earth -> 1.0
-    | Mars -> 1.8808158
-    | Jupiter -> 11.862615
-    | Saturn -> 29.447498
-    | Uranus -> 84.016846
-    | Neptune -> 164.79132
-    |> (*) 31557600.0
-    |> (/) (float seconds)
+    | Mercury -> 0.2408467<earthYear>
+    | Venus -> 0.61519726<earthYear>
+    | Earth -> 1.0<earthYear>
+    | Mars -> 1.8808158<earthYear>
+    | Jupiter -> 11.862615<earthYear>
+    | Saturn -> 29.447498<earthYear>
+    | Uranus -> 84.016846<earthYear>
+    | Neptune -> 164.79132<earthYear>
+    |> (*) 31557600.0<second/earthYear>
+    |> (/) ((float seconds) * 1.0<second>)
