998e26990f0b6d5ad703ad1f676bf48b9df157ba
Pratik Desai <pratik06@gmail.com>
2021-12-11 22:48:37 -0800

    Added solution for lucians luscious lasagna in clojure track.

---
+++ clojure/lucians-luscious-lasagna/project.clj
@@ -0,0 +1,5 @@
+(defproject lucians-luscious-lasagna "0.1.0-SNAPSHOT"
+  :description "lucians-luscious-lasagna exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/lucians-luscious-lasagna"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
